fad45b52adcac37a4790dc1e497010e96b1350f4
Peter Tao <petertao95@gmail.com>
2022-09-23 17:30:35 -0700

    Initial Commit

---
+++ facedetectissue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  facedetectissue
+//
+//  Created by petertao on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
